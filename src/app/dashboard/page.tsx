16add149efb58d2a0546454c1a4a0ada6a853531
nelson kenzo tamashiro <nelsonkenzotamashiro@gmail.com>
2025-02-12 08:40:19 -0300

    feat: Add feedback section to dashboard with Google Forms integration
    - Introduced new card for user feedback on the dashboard
    - Implemented Dialog component to trigger feedback form
    - Embedded Google Forms iframe for collecting user suggestions and bug reports
    - Enhanced user engagement with easy feedback submission

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,21 @@
 
 import type React from "react";
 import { PrivateLayout } from "~/components/private-layout"; // Importando o layout privado
+import { Button } from "~/components/ui/button";
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "~/components/ui/card"; // Importando o componente Card do Shadcn
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Dialog } from "~/components/ui/dialog";
 import PageHeader from "~/components/ui/page-header";
 
 const DashboardPage: React.FC = () => {
@@ -54,6 +63,34 @@ const DashboardPage: React.FC = () => {
             RichText e pode ser feito com texto formatado, podem ser incluidas
             imagens e videos do youtube.
           </CardDescription>
+        </CardHeader>
+      </Card>
+
+      <Card>
+        <CardHeader className="flex flex-col sm:flex-row justify-between gap-4">
+          <div className="flex flex-col gap-2">
+            <CardTitle className="text-xl">Feedbacks</CardTitle>
+            <CardDescription className="flex flex-col gap-2">
+              Você tem uma sugestão de melhoria? Algo que gostaria de ver no
+              Neo-news? Quer relatar um problema? Deixe seu feedback aqui.
+            </CardDescription>
+          </div>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>Deixe seu feedback</Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-screen w-fit [&>button]:hidden">
+              <iframe
+                title="Formulário de Feedback"
+                src="https://docs.google.com/forms/d/e/1FAIpQLSeYf5ezlSSVRv5Wd1FPM8AHkzHwDyx_8644CBH0aO5SZznJiw/viewform?embedded=true"
+                width="640"
+                height="813"
+              >
+                Carregando…
+              </iframe>
+            </DialogContent>
+          </Dialog>
         </CardHeader>
       </Card>
     </PrivateLayout>
